11a17b3c2ac40c3dbc8c76f8a000eb44954bf9cb
kchyzhyk <kchyzhykova888@gmail.com>
2024-11-15 21:10:03 +0100

    .

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,6 @@
 export default {
     base: '/aetherTest/',
     build: {
-        outDir: 'dist',
-        rollupOptions: {
-            external: ['three'],
-        },
+        outDir: 'dist'
     }
 };
